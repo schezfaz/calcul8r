951bb2a5fe87b0634b5977c59283248df69935dd
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2019-01-24 20:14:38 +0530

    added calculator functionality, except backspace

---
+++ main/calculator.js
@@ -0,0 +1,43 @@
+var calcApp = angular.module('calculatorApp', []);
+function calculatorController($scope){
+    $scope.output = "0";  //result
+    $scope.isCalculating = false; //to check whether an operator is in operating state or not 
+    $scope.operand = 0; //stores the operand
+
+    $scope.updateOutput = function(btn){
+        if($scope.output == "0"){
+            $scope.output = btn;
+        }
+        else{
+            $scope.output += String(btn);
+        }
+    };
+
+    $scope.operate = function(op) {
+        if($scope.output && !$scope.isCalculating){
+            $scope.operand = $scope.output;
+            $scope.output += op;
+            $scope.isCalculating = true;
+        }else if($scope.output.length > $scope.operand.length+1){  
+            $scope.output = eval($scope.output);  //evaluates string value
+            $scope.output += op;
+        } 
+    };
+
+    $scope.equal = function() {
+        if($scope.output.length > $scope.operand.length+1){
+            $scope.output = eval($scope.output);
+            $scope.operand = $scope.output;
+        }else{
+            $scope.output = $scope.operand;
+        }
+    };
+
+    // $scope.backspace = function(){
+    //     if($scope.length>0){
+    //         $scope.output = $scope.output.substr(0, $scope.output.length-1);
+    //     }
+    // };
+            
+}
+    calcApp.controller('calculatorController', calculatorController);
